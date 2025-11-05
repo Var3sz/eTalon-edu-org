7552352557e13b4e9ef8baab951dd66a243703f8
Var3sz <varga.adam011208@gmail.com>
2025-11-05 21:50:44 +0100

    fix: SESSION kezelés fix

--- apps/frontend/src/components/providers.tsx
+++ apps/frontend/src/components/providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import React, { ReactNode } from 'react';
 
@@ -39,8 +40,8 @@ export function ClientProvider(props: { children: React.ReactNode }) {
   );
 }
 
-const Providers = ({ children }: { children: ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, session }: { children: ReactNode; session: Session | null }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Providers;
